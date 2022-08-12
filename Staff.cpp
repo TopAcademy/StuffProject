8bc7af68e051918cf852e53f1b119349d367e3a6
Max Huzmiev <max@huzm.ru>
2022-08-12 14:08:34 +0300

    Step 5. The team and the job

--- Staff.cpp
+++ Staff.cpp
@@ -4,6 +4,8 @@
  массива объектов "Персонал"
 */
 #include <iostream>
+#include <vector>
+#include "Staff.h"
 #include "Chief.h"
 #include "Engineer.h"
 #include "Service.h"
@@ -11,11 +13,18 @@
 
 int main()
 {
-    top::Chief gen_dir("Steve Bobs", "General director");
-    top::Engineer modeler("Iron Mask", "Construction modeler");
-    top::Service cleaner("Nancy Perochi", "Floors washer");
-    gen_dir.do_the_job();
-    modeler.do_the_job();
-    cleaner.do_the_job();
-}
+	std::vector<top::Staff*> the_team;
+	// gathering the team
+	the_team.push_back(new top::Chief("Donald Dump", "CEO"));
+	the_team.push_back(new top::Chief("Bill Greeks", "CTO"));
+	the_team.push_back(new top::Engineer("Nikola Kresla", "Powerman"));
+	the_team.push_back(new top::Engineer("Thomas Bedisson", "Mechanics"));
+	the_team.push_back(new top::Service("James Rond", "Sequrity man"));
+	the_team.push_back(new top::Service("Joe Briden", "Windows washer"));
+	// now let's do the job
+	for (int i = 0; i < the_team.size(); i++) {
+		the_team[i]->do_the_job();
+	}
 
+
+}
